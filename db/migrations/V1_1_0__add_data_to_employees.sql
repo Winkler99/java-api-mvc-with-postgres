2e1dce2c64160a928a25d830fab9738aec8e3a0f
Wilmer Winkler <wilmer.winkler@se.experis.com>
2025-01-23 13:53:50 +0100

    Core complete!

---
+++ db/migrations/V1_1_0__add_data_to_employees.sql
@@ -0,0 +1,4 @@
+insert into employees(name, jobName, salaryGrade, department) VALUES('John', 'Developer', '7', 'IT');
+insert into employees(name, jobName, salaryGrade, department) VALUES('Jonathan', 'Admin', '8', 'IT');
+insert into employees(name, jobName, salaryGrade, department) VALUES('Johnny', 'Developer', '7', 'IT');
+insert into employees(name, jobName, salaryGrade, department) VALUES('Johan', 'Idk', '6', 'HR');
